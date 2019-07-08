7ee773a4677ec8d5ba89f65f4c518dc653a03f07
hw <hw@lanqiao.org>
2019-07-08 15:56:59 +0800

    新增输出内容

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,4 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld.....");
         System.out.println("git hub ......");
-
-    }
-}
+    }}
